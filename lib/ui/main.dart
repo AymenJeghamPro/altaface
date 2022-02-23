47f4da69df2d978e168d0c02856f8039c28f0124
Med Amine Azaiez <Damin.azaiez.97@gmail.com>
2022-02-23 23:27:59 +0100

    adding certif to work on lower android versions

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'main/views/main_screen.dart';
 
-void main() => runApp(const AltaFaceApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ByteData data =
+      await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext
+      .setTrustedCertificatesBytes(data.buffer.asUint8List());
+  runApp(const AltaFaceApp());
+}
 
 class AltaFaceApp extends StatefulWidget with WidgetsBindingObserver {
   const AltaFaceApp({Key? key}) : super(key: key);
